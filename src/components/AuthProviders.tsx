245bc72b969dbce01fed063cdf2955cad9d07b7b
Nataliia Lisova <nglisova@gmail.com>
2023-08-02 17:32:26 +0300

    navigation

---
+++ src/components/AuthProviders.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthProviders = () => {
+
+    return (
+        <div>
+            Auth
+        </div>)
+}
+
+export default AuthProviders;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {NavLinks} from "@/constants";
+import AuthProviders from "@/components/AuthProviders";
+
+const Navbar = () => {
+    const session={};
+    return (
+        <nav className="flexBetween navbar">
+            <div className="flex-1 flexStart gap-10">
+                <Link href="/">
+                    <Image
+                        src="/logo.svg"
+                        alt="Stand with Ukraine logo"
+                        width={120}
+                        height={40}
+                    />
+                </Link>
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className="flexCenter gap-4">
+                {session ? (
+                    <>
+                        UserPhoto
+
+                        <Link href="/create-project">
+                            Share Work
+                        </Link>
+                    </>
+                ):(
+                    <AuthProviders/>
+                )}
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
