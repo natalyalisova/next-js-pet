4b3971c4d3c42041d53f69392160db35d62ed6ce
Nataliia Lisova <nglisova@gmail.com>
2023-09-10 14:14:27 +0300

    feature: Add styles to "sign in" button

--- src/components/AuthProviders.tsx
+++ src/components/AuthProviders.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import {getProviders,signIn} from "next-auth/react";
-import {useState, useEffect} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowRightToBracket, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {getProviders, signIn} from "next-auth/react";
+import {useEffect, useState} from "react";
+import Button from "@/components/Buttton";
 
 
 type Provider = {
@@ -20,27 +19,32 @@ type Providers = Record<string, Provider>
 const AuthProviders = () => {
     const [providers, setProviders] = useState<Providers | null>(null);
 
-useEffect(() => {
-    const fetchProviders = async () =>{
-        const response = await getProviders();
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const response = await getProviders();
 
-        console.log("response: ", response);
+            console.log("response: ", response);
 
-        setProviders(response);
+            setProviders(response);
+        }
+        fetchProviders();
+    }, [])
+
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <Button
+                        key={i}
+                        handleClick={() => signIn(provider?.id)}
+                        title="Sign In"
+                    />
+
+
+                ))}
+            </div>)
     }
-    fetchProviders();
-}, [])
-
-
-if (providers){
-    return (
-        <div>
-            {Object.values(providers).map((provider: Provider, i) => (
-                <button key={i} onClick={() => signIn(provider?.id)}>
-                    <FontAwesomeIcon icon={faArrowRightToBracket}/><span className="ml-3">{provider.id}</span> </button>
-            ))}
-        </div>)
-}
 
 
 }
